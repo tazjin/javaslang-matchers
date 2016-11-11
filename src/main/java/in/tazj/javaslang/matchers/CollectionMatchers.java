c38006c362f2f26d039e3c6295b19be0c0777482
Vincent Ambo <tazjin@gmail.com>
2016-11-11 18:05:11 -0500

    chore collections: Deprecated containsElement
    Deprecates the containsElement matcher in favour of containsAny(is(element)).

--- src/main/java/in/tazj/javaslang/matchers/CollectionMatchers.java
+++ src/main/java/in/tazj/javaslang/matchers/CollectionMatchers.java
@@ -6,6 +6,8 @@ import org.hamcrest.TypeSafeMatcher;
 
 import javaslang.collection.Traversable;
 
+import static org.hamcrest.Matchers.is;
+
 /**
  * Provides Hamcrest matchers for Javaslang collection types.
  */
@@ -89,20 +91,13 @@ public class CollectionMatchers {
   /**
    * Matches a Javaslang {@link Traversable} that contains a certain element.
    *
+   * This matcher is deprecated and you should use containsAny(is(element)) instead.
+   *
    * @param element The expected element.
    */
+  @Deprecated
   public static <E, T extends Traversable<E>> Matcher<T> containsElement(E element) {
-    return new TypeSafeMatcher<T>() {
-      @Override
-      protected boolean matchesSafely(T es) {
-        return es.contains(element);
-      }
-
-      @Override
-      public void describeTo(Description description) {
-        description.appendText("Collection should contain ").appendValue(element);
-      }
-    };
+    return containsAny(is(element));
   }
 
   /**
